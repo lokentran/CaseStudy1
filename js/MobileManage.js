3e2c5f34d84bce9e94afa1bd0fafa7bf28c10fa1
lokentran <thanhtungmarine@gmail.com>
2020-06-14 21:52:12 +0700

    Chức năng thêm điện thoại: update lớp MobileManage.js

--- js/MobileManage.js
+++ js/MobileManage.js
@@ -13,5 +13,35 @@ class MobileManage {
         }
     }
 
+    addMobile(mobile){
+        this.mobiles.push(mobile);
+    }
 
+    getHtml(){
+        let table = `<table border="0px solid black" cellspacing="0" class="table table-striped">
+                        <tr>
+                            <th>Image</th>
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th>Color</th>
+                            <th>Status</th>
+                            <th>Category</th>
+                            <th>Description</th>
+                            <th colspan ="2" >Action</th>
+                        </tr>`;
+        for (let i = 0; i < this.mobiles.length; i++) {
+            table += "<tr>"+this.mobiles[i].getHtml() + this.getAction(i)+'</tr>';
+        }
+        table+=`</table>`;
+        return table;
+    }
+    getAction(index){
+        let action = `<td><button class="btn btn-warning" onclick="edit(${index})" data-toggle="modal" data-target="#myModal">Edit</button></td>
+                        <td><button class="btn btn-danger" onclick="del(${index})">Delete</button></td>`;
+        return action;
+    }
+
+    getMobileByIndex(index){
+        return this.mobiles[index];
+    }  
 }    
